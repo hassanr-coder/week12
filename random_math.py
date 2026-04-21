fbef82a810e55f9f1dc18e4146fdd7c706672e05
hassanr-coder <hassanr@students.trinitydc.edu>
2026-04-21 19:03:44 -0400

    create

---
+++ random_math.py
@@ -0,0 +1,19 @@
+'''
+Rayaan Hassan
+CMSC 111
+Spring 2026
+Week 12 Assignment 1
+Code Helper - Gemini Google Ai
+'''
+
+import random
+import math
+
+# Using a loop to generate 5 random integers
+for i in range(5):
+    # Generate a random integer between 1 and 100
+    number = random.randint(1, 100)
+# Calculate the square root of the number
+    square_root = math.sqrt(number)
+# Prints the results and square root on the same line 
+    print(f"Number: {number}, Square Root: {square_root}")
